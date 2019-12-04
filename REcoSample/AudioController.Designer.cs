97cb6c9091f48d75cb5627cb19fc95bdedd28439
Daniel <daniel.fo.96@gmail.com>
2019-12-04 18:23:25 +0100

    Ajustada la ventana

--- REcoSample/AudioController.Designer.cs
+++ REcoSample/AudioController.Designer.cs
@@ -35,18 +35,20 @@ namespace SpeechDIM
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(34, 76);
+            this.label1.Location = new System.Drawing.Point(45, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 44);
+            this.label1.Size = new System.Drawing.Size(0, 54);
             this.label1.TabIndex = 0;
             // 
-            // Form1
+            // AudioController
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 235);
+            this.ClientSize = new System.Drawing.Size(1044, 289);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "AudioController";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.AudioController_Load);
             this.ResumeLayout(false);
